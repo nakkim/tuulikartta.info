5070b0da68b2070eb4650387071592a074a810f8
Ville Ilkka <ville.iv.ilkka@gail.com>
2020-11-03 22:00:37 +0200

    Add &timestep=10 to filter out 1 minute observations which cause problems on data queries

--- php/getdata.php
+++ php/getdata.php
@@ -14,6 +14,7 @@ $settings["stationtype"]    = "synop";
 $settings["parameters"]     = "ri_10min,ws_10min,wg_10min,wd_10min,vis,wawa,t2m,n_man,r_1h,snow_aws,pressure,rh";
 $settings["storedquery_id"] = "fmi::observations::weather::multipointcoverage";
 $settings["bbox"]           = "16.58,58.81,34.8,70.61,epsg::4326";
+$settings["timestep"]       = "10";
 $synopdata = $dataMiner->multipointcoverage($timestamp, $settings, false);
 
 $synopdata = $dataMiner->serializeData($synopdata);
